c0618992f6ea24854fb215a9185aaa5b4d80a505
Haidamac <serge.krinitsa@gmail.com>
2023-03-04 22:25:28 +0200

    model Event updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_194043) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_201941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_194043) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
+    t.date "date_start"
+    t.date "date_end"
   end
 
   create_table "places", force: :cascade do |t|
