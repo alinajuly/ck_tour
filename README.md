d26ffcfcefa2053e03952ce5c3a1dc9d58784a50
alinajuly <bluealinajuly90@gmail.com>
2023-03-07 19:21:51 +0200

    edit readme file and gitignore

--- README.md
+++ README.md
@@ -1,7 +1,73 @@
-# README
+# API application Tourist portal of Cherkasy region
 
-Tourist portal of Cherkasy region
+GeekHub graduation project
 
-GeekHub Ruby on Rails graduation project
+## How to install
 
-by [Alina Cheriapkina](https://github.com/alinajuly) & [Sergii Krynytsia](https://github.com/Haidamac)
+* Use git clone to clone repository - type in console
+
+```
+git clone git@github.com:alinajuly/ck_tour.git
+```
+
+* Install Ruby version '3.1.2' use instruction
+
+```
+https://www.ruby-lang.org/en/documentation/installation/
+```
+
+You may already have Ruby installed on your computer. You can check in console by typing:
+
+```
+ruby -v
+```
+
+* Install the required gems (Gemfile) in ck_tour derictory (type in console)
+
+```
+gem install bundler
+```
+
+```
+bundle install
+```
+
+* Type in console:
+
+```
+rails db:create
+```
+
+```
+rails db:migrate
+```
+
+```
+rails db:seed
+```
+
+## How to open in browser
+
+type in console
+
+```
+rails s
+```
+
+visit
+
+```
+http://localhost:3000
+```
+
+## Notice
+
+* After each command `git pull` please type in console
+
+```
+bundle install
+```
+
+```
+rails db:migrate
+```
--- db/schema.rb
+++ db/schema.rb
@@ -26,18 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_214901) do
     t.decimal "longitude", precision: 10, scale: 6
   end
 
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.integer "status"
@@ -48,6 +36,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_214901) do
     t.bigint "user_id", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "places", force: :cascade do |t|
