754835644126393a28f7fa82951c09b0a87cbac7
Haidamac <serge.krinitsa@gmail.com>
2023-03-14 00:55:18 +0200

    code refactoring

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -1,6 +1,6 @@
 class Accommodation < ApplicationRecord
   has_many :rooms
-  has_one :coordinate, as: :coordinatable
+  has_many :coordinates, as: :coordinatable
   has_many_attached :image do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
     attachable.variant :main, resize_to_limit: [900, 900]
