d96e8156524b6cfbc069f43e062a1fcf41e0f3cd
alinajuly <bluealinajuly90@gmail.com>
2023-03-13 22:47:36 +0200

    added partners routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       post '/auth/login', to: 'authentication#login'
+      resources :partners
+      post '/auth/login', to: 'authentication#login'
       resources :tours
       resources :comments
       resources :rates
