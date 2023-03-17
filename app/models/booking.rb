d802fccb7d4d4e2d14c2896d1aeeec0b84081ba5
Haidamac <serge.krinitsa@gmail.com>
2023-03-17 16:19:01 +0200

    fix bug with enough places in booking system

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,10 +4,11 @@ class Booking < ApplicationRecord
 
   enum confirmation: { pending: 0, approved: 1, cancelled: 2 }
 
-  validates :check_in, :check_out, presence: true
+  validates :check_in, :check_out, :number_of_peoples, presence: true
   validates :check_out, comparison: { greater_than: :check_in }
   validate :not_in_past
   validate :check_availability, on: :create
+  validate :enough_places, on: :create
 
   def not_in_past
     errors.add(:check_in, 'can\'t be in the past') if check_in.present? && check_in < Date.today
@@ -15,13 +16,20 @@ class Booking < ApplicationRecord
     return unless check_out.present? && check_out < Date.today
 
     errors.add(:check_out, 'can\'t be in the past')
-
   end
 
   def check_availability
-    return unless room.bookings.where.not(id:).where(check_in: ..check_out, check_out: check_in..).any? && busy_rooms >= room.quantity
+    return unless room.bookings.where.not(id:)
+                      .where(check_in: ..check_out, check_out: check_in..)
+                      .any? && busy_rooms >= room.quantity
 
     errors.add(:room, 'is not available on the selected dates')
+  end
+
+  def enough_places
+    return unless number_of_peoples > room.places
+
+    errors.add(:room, 'is not enough places in the selected room')
   end
 
   def busy_rooms
