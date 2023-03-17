34fcc22654b96c8b752ac4a5df6b6fa37fe7b77d
alinajuly <bluealinajuly90@gmail.com>
2023-03-18 01:34:23 +0200

    create swagger documentation for accomodations, authentication, users

--- spec/requests/api/v1/authentication_spec.rb
+++ spec/requests/api/v1/authentication_spec.rb
@@ -1,7 +1,37 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe "Api::V1::Authentications", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'api/v1/authentication', type: :request do
+  path '/api/v1/auth/login' do
+    post('authentication user') do
+      tags 'Authentication'
+      description 'authentication user'
+      consumes 'application/json'
+      parameter name: :user,
+                in: :body,
+                required: true,
+                schema: {
+                  type: :object,
+                  properties: {
+                    email: { type: :string },
+                    password: { type: :string }
+                  },
+                  required: [ :email, :password ]
+                }
+      
+      response(201, 'successful') do
+        it 'should returns status response' do
+          expect(response.status).to eq(201)
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,159 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'api/v1/users', type: :request do
+  path '/api/v1/users' do
+    get('list users') do
+      tags 'User'
+      produces 'application/json'
+
+      response(200, 'successful') do
+        it 'should returns status response' do
+          expect(response.status).to eq(200)
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create user') do
+      tags 'User'
+      description 'Creates a new user'
+      consumes 'application/json'
+      parameter name: :user,
+                in: :body,
+                required: true,
+                schema: {
+                  type: :object,
+                  properties: {
+                    name: { type: :string },
+                    email: { type: :string },
+                    password: { type: :string }
+                  },
+                  required: [ :name, :email, :password ]
+                }
+      
+      response(201, 'successful') do
+        it 'should returns status response' do
+          expect(response.status).to eq(201)
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: :id, in: :path, type: :string, description: 'user id'
+
+    get('show user') do
+      tags 'User'
+    
+      response(200, 'successful') do
+        it 'should returns status response' do
+          expect(response.status).to eq(200)
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update user') do
+      tags 'User'
+      consumes 'application/json'
+      parameter name: :user,
+                in: :body,
+                required: true,
+                schema: {
+                  type: :object,
+                  properties: {
+                    name: { type: :string },
+                    email: { type: :string },
+                    password: { type: :string }
+                  },
+                  required: [ :name, :email, :password ]
+                }
+      
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update user') do
+      tags 'User'
+      consumes 'application/json'
+      parameter name: :user,
+                in: :body,
+                required: true,
+                schema: {
+                  type: :object,
+                  properties: {
+                    name: { type: :string },
+                    email: { type: :string },
+                    password: { type: :string }
+                  },
+                  required: [ :name, :email, :password ]
+                }
+
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete user') do
+      tags 'User'
+      
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
