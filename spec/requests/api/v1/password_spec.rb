d9e8782984d8623e57ba54f64a097228b86d5d44
Haidamac <serge.krinitsa@gmail.com>
2023-03-18 12:27:33 +0200

    swagger docs for password added

---
+++ spec/requests/api/v1/password_spec.rb
@@ -0,0 +1,64 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/password', type: :request do
+
+  path '/api/v1/password/forgot' do
+
+    post('forgot password') do
+      tags 'Authentication'
+      description 'forgot users password'
+      consumes 'application/json'
+      parameter name: :user,
+                in: :body,
+                required: true,
+                schema: {
+                  type: :object,
+                  properties: {
+                    email: { type: :string },
+                  },
+                  required: [ :email ]
+                }
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/password/reset' do
+
+    post('reset password') do
+      tags 'Authentication'
+      description 'reset password'
+      consumes 'application/json'
+      parameter name: :user,
+                in: :body,
+                required: false,
+                schema: {
+                  type: :object,
+                  properties: {
+                    email: { type: :string },
+                    password: { type: :string }
+                  }
+                }
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
