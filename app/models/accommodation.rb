89d12e4c5e6cdcc31671eb9e638de9ebe3f547d1
alinajuly <bluealinajuly90@gmail.com>
2023-03-21 18:58:44 +0200

    edit syntax

--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -7,6 +7,7 @@ class Accommodation < ApplicationRecord
     attachable.variant :thumb, resize_to_limit: [100, 100]
     attachable.variant :main, resize_to_limit: [900, 900]
   end
+  
   enum status: { unpublished: 0, published: 1 }
 
   scope :toponym_filter, ->(locality) { joins(:toponyms).where('locality ILIKE ?', "%#{locality}%") }
