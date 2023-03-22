6fc749d05ded56bd8bce3bf59ed9ac7822abd300
alinajuly <bluealinajuly90@gmail.com>
2023-03-22 23:17:52 +0200

    change url

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://164.92.135.103/',
+          url: '164.92.135.103',
           variables: {
             defaultHost: {
               default: 'localhost:3000/'
