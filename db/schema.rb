4e5e414aee8546370bfdcc26883341cdc3c4d45a
alinajuly <bluealinajuly90@gmail.com>
2023-04-08 02:43:09 +0300

    resolve problem with migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_07_233435) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_212401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -239,6 +239,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_233435) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.integer "role", default: 0
+    t.string "stripe_id"
   end
 
   add_foreign_key "accommodations", "users"
