c8e611e078a3a73ca1bb422af72d27c53040b26e
Haidamac <serge.krinitsa@gmail.com>
2023-04-20 23:39:52 +0300

    attach logo to mailers

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,4 +1,6 @@
 class BookingMailer < ApplicationMailer
+  before_action :logo_attach
+
   def booking_confirmation(user, booking)
     @user = user
     @booking = booking
@@ -12,5 +14,11 @@ class BookingMailer < ApplicationMailer
     @accommodation = @booking.room.accommodation
     @geolocation = Geolocation.find_by(geolocationable_id: @accommodation.id, geolocationable_type: 'Accommodation')
     mail(to: @user.email, subject: 'Ваше бронювання')
+  end
+
+  private
+
+  def logo_attach
+    attachments.inline['logo.svg'] = File.read((Rails.root.join('public', 'logo.svg')))
   end
 end
