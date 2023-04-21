c5ca50cb2984630d6b171ee1b73f2432b9517a1c
alinajuly <bluealinajuly90@gmail.com>
2023-04-21 15:41:04 +0300

    update spec

---
+++ spec/requests/api/v1/plans_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/plans_controller', type: :request do
+
+  path '/api/v1/plans' do
+    get('plans for authenticated user') do
+      tags 'Plans'
+      produces 'application/json'
+      security [ jwt_auth: [] ]
+
+      response(200, 'successful') do
+        it 'should returns status response' do
+          expect(response.status).to eq(200)
+        end
+      end
+
+      response(401, 'unauthorized') do
+        it 'should returns status response' do
+          expect(response.status).to eq(401)
+        end
+      end
+
+      response(404, 'not found') do
+        it 'should returns status response' do
+          expect(response.status).to eq(404)
+        end
+      end
+
+      response(422, 'invalid request') do
+        it 'should returns status response' do
+          expect(response.status).to eq(422)
+        end
+      end
+    end
+  end
+end
