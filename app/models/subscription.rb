87017a6816116ebdc7083bd3404a61c9a3ca1855
alinajuly <bluealinajuly90@gmail.com>
2023-04-26 23:34:02 +0300

    fixed puma error

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ApplicationRecord
   belongs_to :user
 
-  validates :plan_id, :customer_id, :status, :current_period_end, :current_period_start, :interval, :subscription_id
+  validates :plan_id, :customer_id, :status, :current_period_end, :current_period_start, :interval, :subscription_id, presence: true
 end
