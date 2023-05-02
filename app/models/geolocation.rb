bbb445cc5f0fe7c7cbaee74756988ab0ffd6ec8b
alinajuly <bluealinajuly90@gmail.com>
2023-05-02 17:08:16 +0300

    add global const to Geolocation

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,8 +1,12 @@
 class Geolocation < ApplicationRecord
   belongs_to :geolocationable, polymorphic: true
 
-  LATT = { min_latt: -90, max_latt: 90 }
-  validates :latitude, presence: true, numericality: { greater_than_or_equal_to: LATT[:min_latt], less_than_or_equal_to: LATT[:max_latt] }
-  validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  LATTITUDE = { min_lattitude: -90, max_lattitude: 90 }
+  LONGITUDE = { min_longitude: -180, max_longitude: 180 }
+
+  validates :latitude, presence: true, numericality: { greater_than_or_equal_to: LATTITUDE[:min_lattitude], 
+                                                       less_than_or_equal_to: LATTITUDE[:max_lattitude] }
+  validates :longitude, presence: true, numericality: { greater_than_or_equal_to: LONGITUDE[:min_longitude], 
+                                                        less_than_or_equal_to: LONGITUDE[:max_longitude] }
   validates :locality, presence: true
 end
