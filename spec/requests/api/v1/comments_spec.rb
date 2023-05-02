69e3c388e4c2ca7a1ea464c9030062106dd4cf86
Haidamac <serge.krinitsa@gmail.com>
2023-05-02 09:17:54 +0300

    change test to comments

--- spec/requests/api/v1/comments_spec.rb
+++ spec/requests/api/v1/comments_spec.rb
@@ -93,41 +93,34 @@ RSpec.describe 'api/v1/comments', type: :request do
     parameter name: 'parentable_type', in: :path, type: :string, description: 'f.e. attractions, accommodations, catering, tour'
     parameter name: 'parentable_id', in: :path, type: :string, description: 'f.e. attraction_id, accommodation_id, catering_id, tour_id'
     parameter name: :id, in: :path, type: :string, description: 'comment id'
+    let(:parentable_type) { 'attractions' }
+    let(:parentable_id) { attraction.id }
+    let!(:comment) { create(:comment, commentable_id: attraction.id, user_id: user.id) }
+    let(:token) { JWT.encode({ user_id: admin.id }, Rails.application.secret_key_base) }
+    let(:headers) { { 'Authorization' => "Bearer #{token}" } }
+    let(:Authorization) { headers['Authorization'] }
 
     get('show comment for all') do
       tags 'Comment'
       security [ jwt_auth: [] ]
 
       response(200, 'successful') do
-        let(:comment_id) { '123' }
-        let(:id) { '123' }
+        let(:id) { comment.id }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-
-      response(200, 'successful') do
         it 'should returns status response' do
           expect(response.status).to eq(200)
         end
+
+        run_test!
       end
 
       response(404, 'not found') do
+        let(:id) { 'invalid' }
         it 'should returns status response' do
           expect(response.status).to eq(404)
         end
-      end
 
-      response(422, 'invalid request') do
-        it 'should returns status response' do
-          expect(response.status).to eq(422)
-        end
+        run_test!
       end
     end
 
@@ -146,35 +139,41 @@ RSpec.describe 'api/v1/comments', type: :request do
                 }
 
       response(200, 'successful') do
-        let(:geolocation_id) { '123' }
-        let(:id) { '123' }
+        let(:id) { comment.id }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        before do
+          puts attraction.inspect
+          puts comment.inspect
         end
-        run_test!
-      end
 
-      response(401, 'unauthorized') do
-        it 'should returns status response' do
-          expect(response.status).to eq(401)
+        it 'returns a 200 response' do
+          expect(response).to eq(200)
+        end
+
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          comment.update(status: 'published')
+          expect(Comment.find_by(status: 'published')).to eq(comment)
         end
       end
 
       response(404, 'not found') do
+        let(:id) { 'invalid' }
         it 'should returns status response' do
           expect(response.status).to eq(404)
         end
+
+        run_test!
       end
 
       response(422, 'invalid request') do
+        let(:id) { comment.id }
         it 'should returns status response' do
+          comment.update(body: 'published')
           expect(response.status).to eq(422)
         end
+
+        run_test!
       end
     end
 
@@ -182,42 +181,32 @@ RSpec.describe 'api/v1/comments', type: :request do
       tags 'Comment'
       security [ jwt_auth: [] ]
 
-      response(200, 'successful') do
-        let(:comment_id) { '123' }
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+      response(204, 'no content') do
+        let(:id) { comment.id }
+        it 'should returns status response' do
+          expect(response.status).to eq(204)
         end
+
         run_test!
       end
 
-      response(200, 'successful') do
-        it 'should returns status response' do
-          expect(response.status).to eq(200)
-        end
-      end
-
       response(401, 'unauthorized') do
+        let(:id) { comment.id }
+        let(:Authorization) { nil }
         it 'should returns status response' do
           expect(response.status).to eq(401)
         end
+
+        run_test!
       end
 
       response(404, 'not found') do
+        let(:id) { 'invalid' }
         it 'should returns status response' do
           expect(response.status).to eq(404)
         end
-      end
 
-      response(422, 'invalid request') do
-        it 'should returns status response' do
-          expect(response.status).to eq(422)
-        end
+        run_test!
       end
     end
   end
